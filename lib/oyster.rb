03343a1cac4f31e5c76c2ff6877c2614a69616a4
joemaidman <joemaidman@gmail.com>
2017-02-20 18:50:24 +0000

    Added touch-in and out

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -4,6 +4,7 @@ LIMIT = 90
 
   def initialize(balance = 0)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(money)
@@ -14,6 +15,20 @@ LIMIT = 90
 
   def deduct(money)
     @balance -= money
+  end
+
+  def touch_in
+    raise "Card already in journey" if in_journey?
+    @in_journey = true
+  end
+
+  def touch_out
+    raise "Card is not in a journey" unless in_journey?
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 attr_reader :balance
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -2,9 +2,9 @@ require 'oyster'
 
 describe Oystercard do
   subject(:card) {described_class.new}
-# Write an RSpec test for the Oystercard class that will test that a freshly
-# initialized card has a balance of 0 by default, see it fail, then write an
-# implementation (Oystercard class code) that will make the test pass.
+  # Write an RSpec test for the Oystercard class that will test that a freshly
+  # initialized card has a balance of 0 by default, see it fail, then write an
+  # implementation (Oystercard class code) that will make the test pass.
   describe "the current card balance" do
     it "has a balance of zero" do
       expect(card.balance).to eq 0
@@ -31,4 +31,41 @@ describe Oystercard do
     end
   end
 
+  context "touch-in requires to have a balance" do
+    before :each do
+      card.top_up(Oystercard::LIMIT)
+     end
+
+    describe "#touch in" do
+      it "allows the card to be touched-in" do
+        expect(card.touch_in).to be true
+        #expect{ card.touch_in }.to change{ card.in_journey? }.to true
+      end
+      it "raises an error if the card is already in a journey" do
+        expect{ 2.times{card.touch_in} }.to raise_error("Card already in journey")
+      end
+    end
+  end
+
+  describe "#touch out" do
+    it "allows the card to be touched-out" do
+      expect(card.touch_out).to be false
+      #expect{ card.touch_in }.to change{ card.in_journey? }.to true
+    end
+
+    it "raises an error if the card is not in a journey" do
+      expect{ 2.times{card.touch_out} }.to raise_error("Card is not in a journey")
+    end
+  end
+
+  describe "#in_journey?" do
+    #let(:touch_in) {true}
+    it "returns true if the card is in a journey" do
+      expect{ card.touch_in }.to change{ card.in_journey? }.to true
+    end
+    it "returns false if the card is not in a journey" do
+      card.touch_in
+      expect{ card.touch_out }.to change{ card.in_journey? }.to false
+    end
+  end
 end
